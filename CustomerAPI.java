ad06d42d29b02e0d7c7ef47ee90e82e7db61b64a
Mason Countney <masoncount@gmail.com>
2018-05-08 16:30:29 -0400

    Doc

--- CustomerAPI.java
+++ CustomerAPI.java
@@ -1,13 +1,22 @@
 public interface CustomerAPI{
 
-  public void setFirstName(String firstName);
-  public void setLastName(String lastName);
-  public String getName();
-  public void setAccountNumber(int AccountNumber);
-  public int getAccountNumber();
-  public boolean rentDVD(DVD title);
-  public void returnDVD(String x);
-  public String toString();
-  public String rented();
+  // first name
+  // last name
+  // account number
+  // ArrayList of checked out movies
+
+  public void setFirstName(String firstName);       // - Sets the first name of customer
+  public void setLastName(String lastName);         // - Sets the Last name of customer
+  public String getName();                          // - returns Full name. First Last
+  public void setAccountNumber(int AccountNumber);  // - Sets account number
+  public int getAccountNumber();                    // - returns account number
+  public boolean rentDVD(DVD title);                // - Adds an element to the ArrayList
+                                                    //   of DVDs a customer has checked out
+
+  public void returnDVD(String x);                  // - Takes the movie title as a param
+                                                    //   deletes item from ArrayList
+  public String toString();                         // - Returns Firstname Lastname, Account #
+  public String rented();                           // - Returns a list of the movies rented by
+                                                    //   that customer
 
 }
